46cfe43a937073e04be3d509df5a08bd006d32af
Chris Chua <chris.sirhc@gmail.com>
2021-06-21 23:21:55 -0700

    add uusd balance display

--- src/terra-to-shuttle.tsx
+++ src/terra-to-shuttle.tsx
@@ -1,23 +1,46 @@
-import React, { useState } from "react";
-import { Extension } from "@terra-money/terra.js";
+import React, { useEffect, useState } from "react";
+import { LCDClient, Extension, Coins, Dec } from "@terra-money/terra.js";
 
 type ConnectResponse = {
   address: string
 };
 
+const TERRA_DECIMAL = 1e6;
+
+// connect to soju testnet
+const terra = new LCDClient({
+  URL: 'https://tequila-lcd.terra.dev',
+  chainID: 'tequila-0004',
+});
+
 export function TerraToShuttle() {
   const [wallet, setWallet] = useState<ConnectResponse | null>(null);
+  const [balance, setBalance] = useState<Coins | null>(null);
+
+  useEffect(() => {
+    getBalance();
+  }, [wallet])
+
+  const uusdBal = balance && balance.get("uusd");
 
   return (
     <div>
       <h3>Terra To Shuttle</h3>
-      <button onClick={connect}>
+      <button onClick={connect} disabled={Boolean(wallet)}>
         {
           wallet ?
           `Connected to ${wallet.address}` :
           'Connect to Terra Station'
         }
       </button>
+      {
+        balance && balance.get("uusd") ? (
+          <div>
+            UUSD balance
+            <pre>{ uusdBal && new Dec(uusdBal.amount).div(TERRA_DECIMAL).toString() }</pre>
+          </div>
+        ) : null
+      }
     </div>
   );
 
@@ -27,5 +50,13 @@ export function TerraToShuttle() {
     extension.on('onConnect', (w: ConnectResponse) => {
       setWallet(w);
     });
+  }
+
+  async function getBalance() {
+    if (!wallet) {
+      return;
+    }
+    const balance = await terra.bank.balance(wallet.address);
+    setBalance(balance);
   }
 }
