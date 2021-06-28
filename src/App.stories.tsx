57ddf2a5599550224f8dec94bd904c7411fb49fd
Chris Chua <chris.sirhc@gmail.com>
2021-06-27 22:49:27 -0700

    add AllSteps into the storybook

--- src/App.stories.tsx
+++ src/App.stories.tsx
@@ -4,12 +4,28 @@ import { TerraToShuttle } from "./terra-to-shuttle";
 import { EthSideComponent } from "./eth-side";
 import { EthToTerra } from "./eth-to-terra";
 import { EthWalletConnector } from './EthWalletConnector';
+import { AllSteps } from './ConversionStep';
+import { ChakraProvider } from "@chakra-ui/react";
 
 export default {
   title: 'OldApp',
 };
 
-export function App() {
+export function NewApp() {
+  return (
+    <ChakraProvider>
+      <div className="App">
+        <WalletConnector>
+          <EthWalletConnector>
+            <AllSteps />
+          </EthWalletConnector>
+        </WalletConnector>
+      </div>
+    </ChakraProvider>
+  );
+}
+
+export function OldApp() {
   return (
     <div className="App">
       <WalletConnector>
