dd6b783ab09b16383dc52c24a805ff0476e99895
Chris Chua <chris.sirhc@gmail.com>
2021-06-27 22:51:59 -0700

    cleanup App.tsx of references already tested in storybook

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
 import { WalletConnector } from "./WalletConnector";
-import { TerraToShuttle } from "./terra-to-shuttle";
-import { EthSideComponent } from "./eth-side";
-import { EthToTerra } from "./eth-to-terra";
 import { EthWalletConnector } from './EthWalletConnector';
-import MockDesign from "./MockDesign";
 import { AllSteps } from './ConversionStep';
 
 function App() {
@@ -14,9 +9,6 @@ function App() {
       <WalletConnector>
         <EthWalletConnector>
           <AllSteps />
-          {/* <TerraToShuttle />
-          <EthSideComponent />
-          <EthToTerra /> */}
         </EthWalletConnector>
       </WalletConnector>
     </div>
