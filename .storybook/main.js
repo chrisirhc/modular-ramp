216ca19ff9b9a6e513d3b4b00a57059569f5ab5e
Chris Chua <chris.sirhc@gmail.com>
2021-06-27 22:49:53 -0700

    fix styling issues in storybook
    https://github.com/storybookjs/storybook/issues/7540

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,6 @@
+const path = require('path')
+const toPath = _path => path.join(process.cwd(), _path)
+
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
@@ -7,5 +10,18 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/preset-create-react-app"
-  ]
+  ],
+  // From https://github.com/storybookjs/storybook/issues/7540
+  webpackFinal: async config => {
+    return {
+      ...config,
+      resolve: {
+        ...config.resolve,
+        alias: {
+          ...config.resolve.alias,
+          '@emotion/styled': toPath('node_modules/@emotion/styled'),
+        },
+      },
+    }
+  },
 }
