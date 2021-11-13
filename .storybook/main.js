ade88be0ea387a2c11448022709d43e1b04305b2
Chris Chua <chris.sirhc@gmail.com>
2021-11-13 15:23:23 -0800

    fix storybook to work with wasm/wormhole-sdk

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const toPath = (_path) => path.join(process.cwd(), _path);
+const cracoConfig = require('../craco.config');
 
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
@@ -13,8 +14,18 @@ module.exports = {
   },
   // From https://github.com/storybookjs/storybook/issues/7540
   webpackFinal: async (config) => {
+
+    // Wasm support
+    config = cracoConfig.webpack.configure(config);
+
     return {
       ...config,
+
+      // Workaround for fs not found module errors on @certusone/wormhole-sdk
+      node: {
+        fs: 'empty',
+      },
+
       resolve: {
         ...config.resolve,
         alias: {
