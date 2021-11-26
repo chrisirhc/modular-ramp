7d7e5a94bf15eb865ba9f1bac67ea94230ba5095
Chris Chua <chris.sirhc@gmail.com>
2021-11-25 19:47:01 -0800

    add to execute checkbox for testing wormhole transactions

--- src/steps/Wormhole.stories.tsx
+++ src/steps/Wormhole.stories.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Story, Meta } from "@storybook/react";
 import { WalletConnector } from "../WalletConnector";
 import { StepProps } from "../types";
@@ -10,15 +10,23 @@ export default {
   title: "WormholeBridge",
 } as Meta;
 
-const Template: Story<StepProps> = (args, { argTypes }) => (
-  <ChakraProvider>
-    <div className="App">
-      <WalletConnector>
-        <WormholeBridge {...args} />
-      </WalletConnector>
-    </div>
-  </ChakraProvider>
-);
+const Template: Story<StepProps> = (args, { argTypes }) => {
+  const [toExecute, setToExecute] = useState<boolean>(false);
+  return (
+    <ChakraProvider>
+      <div className="App">
+        <WalletConnector>
+          <WormholeBridge {...args} />
+        </WalletConnector>
+        <input
+          type="checkbox"
+          checked={toExecute}
+          onChange={(event) => setToExecute(event.target.checked)}
+        />
+      </div>
+    </ChakraProvider>
+  );
+};
 
 export const Main = Template.bind({});
 Main.args = {};
