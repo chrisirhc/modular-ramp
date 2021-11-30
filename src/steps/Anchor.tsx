0d200318732e8159add02892303c9971136af9d1
Chris Chua <chris.sirhc@gmail.com>
2021-11-29 22:41:56 -0800

    add withdraw flow

--- src/steps/Anchor.tsx
+++ src/steps/Anchor.tsx
@@ -1,13 +1,4 @@
-import React, {
-  useContext,
-  useEffect,
-  useState,
-  useRef,
-  ChangeEventHandler,
-  useCallback,
-  useMemo,
-  FC,
-} from "react";
+import React, { useContext, useState, useCallback, FC } from "react";
 import {
   Button,
   FormControl,
@@ -15,40 +6,21 @@ import {
   Input,
   InputGroup,
   InputRightElement,
-  Spinner,
-  Select,
   VStack,
-  HStack,
-  Text,
   Tabs,
   TabList,
   Tab,
   TabPanel,
   TabPanels,
 } from "@chakra-ui/react";
-import { utils } from "ethers";
-import { BigNumber, BigNumberish } from "@ethersproject/bignumber";
-import { ContractReceipt } from "@ethersproject/contracts";
-import {
-  Connection,
-  clusterApiUrl,
-  PublicKey,
-  Transaction,
-} from "@solana/web3.js";
 
-import { StepProps } from "../types";
-import {
-  EthereumContext,
-  EthereumContextProps,
-} from "../wallet/MetamaskWalletConnector";
+import { EthereumContext } from "../wallet/MetamaskWalletConnector";
 import { TerraContext, TerraContextProps } from "../TerraWalletConnector";
 import {
   AnchorEarn,
   CHAINS,
   NETWORKS,
   DENOMS,
-  Wallet,
-  MnemonicKey,
 } from "@anchor-protocol/anchor-earn";
 import { Msg } from "@terra-money/terra.js";
 import { postWithFees } from "../operations/terra";
@@ -74,36 +46,49 @@ export const Anchor: FC = () => {
   );
 };
 
-interface DepositArgs {
+function getAnchorEarn(address: string) {
+  return new AnchorEarn({
+    chain: CHAINS.TERRA,
+    network: NETWORKS.BOMBAY_12,
+    address,
+  });
+}
+
+function getCustomBroadcaster({
+  networkType,
+  terraContext,
+  memo,
+}: {
+  networkType: NetworkType;
+  terraContext: TerraContextProps;
+  memo: string;
+}) {
+  return async function customBroadcaster(tx: Msg[]) {
+    const txResult = await postWithFees(networkType, terraContext, tx, memo);
+    return txResult.result.txhash;
+  };
+}
+
+interface OperationArgs {
   amount: string;
   networkType: NetworkType;
   terraContext: TerraContextProps;
 }
-async function deposit({ amount, networkType, terraContext }: DepositArgs) {
+async function deposit({ amount, networkType, terraContext }: OperationArgs) {
   if (!terraContext.address) {
     throw new Error("No address");
   }
 
-  const anchorEarn = new AnchorEarn({
-    chain: CHAINS.TERRA,
-    network: NETWORKS.BOMBAY_12,
-    address: terraContext.address,
-  });
-  await anchorEarn.deposit({
+  const anchorEarn = getAnchorEarn(terraContext.address);
+  return await anchorEarn.deposit({
     currency: DENOMS.UST,
     amount,
-    customBroadcaster,
-  });
-
-  async function customBroadcaster(tx: Msg[]) {
-    const txResult = await postWithFees(
+    customBroadcaster: getCustomBroadcaster({
       networkType,
       terraContext,
-      tx,
-      "Deposit into Anchor"
-    );
-    return txResult.result.txhash;
-  }
+      memo: "Deposit on Anchor",
+    }),
+  });
 }
 
 const Deposit: FC = () => {
@@ -149,8 +134,38 @@ const Deposit: FC = () => {
   );
 };
 
+async function withdraw({ amount, networkType, terraContext }: OperationArgs) {
+  if (!terraContext.address) {
+    throw new Error("No address");
+  }
+
+  const anchorEarn = getAnchorEarn(terraContext.address);
+  return await anchorEarn.withdraw({
+    currency: DENOMS.UST,
+    amount,
+    customBroadcaster: getCustomBroadcaster({
+      networkType,
+      terraContext,
+      memo: "Deposit on Anchor",
+    }),
+  });
+}
+
 const Withdraw: FC = () => {
-  const [amount, setAmount] = useState();
+  const terraContext = useContext(TerraContext);
+  const { networkType } = useContext(EthereumContext);
+  const [amount, setAmount] = useState<string>("0");
+  const onAmountChanged = useCallback<
+    React.ChangeEventHandler<HTMLInputElement>
+  >((event) => {
+    setAmount(event.target.value);
+  }, []);
+  const handleWithdraw = useCallback(() => {
+    if (!networkType) {
+      return;
+    }
+    withdraw({ amount, terraContext, networkType });
+  }, [amount, networkType, terraContext]);
 
   return (
     <VStack>
@@ -164,7 +179,7 @@ const Withdraw: FC = () => {
             min="0"
             value={amount || ""}
             // disabled={isToExecute}
-            // onChange={onAmountChanged}
+            onChange={onAmountChanged}
           />
           <InputRightElement
             pointerEvents="none"
@@ -175,7 +190,7 @@ const Withdraw: FC = () => {
           />
         </InputGroup>
       </FormControl>
-      <Button>Withdraw</Button>
+      <Button onClick={handleWithdraw}>Withdraw</Button>
     </VStack>
   );
 };
