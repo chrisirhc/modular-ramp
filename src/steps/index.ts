77a8c81f9bd0038ba2e919c4106186a4e6612ed0
Chris Chua <chris.sirhc@gmail.com>
2021-12-24 00:13:21 +0800

    add missing step_map

--- src/steps/index.ts
+++ src/steps/index.ts
@@ -3,3 +3,8 @@ import { WormholeBridge } from "./Wormhole";
 import { Anchor } from "./Anchor";
 
 export const STEPS: ReadonlyArray<StepComponent> = [WormholeBridge, Anchor];
+
+export const STEP_MAP: Record<string, StepComponent> = {
+  [WormholeBridge.stepTitle]: WormholeBridge,
+  [Anchor.stepTitle]: Anchor,
+};
